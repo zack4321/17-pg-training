81fbfe97794edd154386cae626bea904d8bf3940
Zack <zack@pixiv.com>
2017-05-01 10:25:12 +0900

    タイムラインの見た目をつくった

---
+++ lecture/hello_world.php
@@ -0,0 +1,34 @@
+<?php
+/* $math = 45;
+$science = 30;
+if($math >= 30 && $science >= 30){
+  if( $math + $science >= 80){
+    print '合格おめでとう！';
+  }
+  else{
+    print '来年頑張れよな！';
+  }
+} */
+/* $math = 39;
+$science = 35;
+if($math >= 30 && $science >= 30 && $math + $science >= 80)
+  {
+    print '合格おめでとう！';
+  }
+  elseif($math >= 30 && $science >= 30 && $math + $science >= 75){
+    print '補欠合格です！連絡を待ってね！';
+  }
+  else{
+    print '来年頑張れよな！';
+  } */
+  /* for($i = 1; $i <= 9; $i++) {
+    for($j = 1; $j <= 9; $j++){
+      $result =$i * $j;
+      print "${result} ";
+      if($j===9){
+        print "\n";
+      }
+    }
+  } */
+  for($i = 1; $i <= 30; $i++)
+    print "{$i}";
