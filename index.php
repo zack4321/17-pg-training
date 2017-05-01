69965db958f4f8d6194803c18767c111bb1eecdc
Zack <zack@pixiv.com>
2017-05-01 12:46:49 +0900

    投稿できるようにした

--- index.php
+++ index.php
@@ -11,3 +11,53 @@ $database = getDatabase();
 ?>
 
 <!-- 下にindex.htmlをコピペして、index.htmlを消そう！ -->
+<html>
+    <head>
+        <title>Yastodon(ヤストドン)</title>
+        <link rel="stylesheet" href="/css/style.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="container myself-container">
+                <div class="myself">
+                    <div class="user-icon"></div>
+                    <div class="user-name"></div>
+                </div>
+                <form enctype="multipart/form-data" method="post" action="/post_toot.php">
+                    <textarea name="text" placeholder="今なにしてる？" required></textarea>
+                    <input type="file" name="image">
+                    <div class="toot-button-container">
+                        <input type="submit" class="toot-button" value="トゥート!">
+                    </div>
+                </form>
+            </div>
+
+            <div class="container toot-container">
+                <div class="label icon-home"><img class="label-icon" src="/img/home.png" width="15" alt="Home - ">ホーム</div>
+                <ul>
+                  <li>
+                    <img width="50" src="https://files.slack.com/files-tmb/T02541Q7U-F55EULAEB-5f5a012488/image_uploaded_from_ios_1024.jpg"/>
+                    <div>
+                      <div class="user-container">
+                        <div class="user-name">zack</div>
+                        <div class="user-id">@zack</div>
+                      </div>
+                      <p>Hello! Zack Japan!</p>
+                    </div>
+                  </li>
+                </ul>
+            </div>
+
+            <div class="container about-container">
+                <div class="label icon-asterisk"><img class="label-icon" src="/img/asterisk.png" width="15" alt="Start - ">スタート</div>
+                <div class="contents">
+                    <p>
+                        Yastodonとは研修のために作られた教育用ソーシャル・ネットワーキング・サービスです。<br>
+                        あなただけの素敵なサービスをここから成長させていってください。
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
--- post_toot.php
+++ post_toot.php
@@ -1,8 +1,14 @@
 <?php
+error_reporting(E_ALL);
 require_once 'functions.php';
 // トゥート投稿
 
 session_start();
 redirectToLoginPageIfNotLoggedIn();
+
+$database = getDatabase();
+$user = $database->query("
+    INSERT INTO `toot`(`user_id`, `text`, `image_file_name`, `created_at`) VALUES (" .$_SESSION['user_id'].",'".$_POST['text']."',0,20170501)
+");
 
 header('Location: /');
